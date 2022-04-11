5ca307164b59776bfff5529e81d06914de3b5242
AndresGutierrez <c0dingw.dre@gmail.com>
2022-04-11 14:55:46 -0700

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Andres Gutierrez on 4/11/22.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
